81b936cf7a66f9f657290e062f6359b06ac215c3
sandeep_chandragiri <sandeep_chandragiri@THBSLAPTOP-804.thbs.india.com>
2021-09-15 20:57:43 +0530

    Initial Java Project

---
+++ src/co/corejava/ipl/app/IPLapp.java
@@ -0,0 +1,10 @@
+package co.corejava.ipl.app;
+
+public class IPLapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("*** Starting IPL APP ***");
+	}
+
+}
